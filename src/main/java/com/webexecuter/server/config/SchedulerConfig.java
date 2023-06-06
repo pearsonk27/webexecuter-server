c4d0ce58fe225776daffb9e5cd995dcb9ee0fa62
pearsonk27 <48700390+pearsonk27@users.noreply.github.com>
2023-06-05 22:37:21 -0400

    Clean up configuration

--- src/main/java/com/webexecuter/server/config/SchedulerConfig.java
+++ src/main/java/com/webexecuter/server/config/SchedulerConfig.java
@@ -1,15 +1,10 @@
 package com.webexecuter.server.config;
 
-import java.io.IOException;
-import java.util.Properties;
-
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.config.PropertiesFactoryBean;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.quartz.QuartzDataSource;
 import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -18,26 +13,30 @@ import org.springframework.jdbc.datasource.init.DatabasePopulator;
 import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
-import org.springframework.scheduling.quartz.SpringBeanJobFactory;
-
 import com.webexecuter.server.job.ScriptJobFactory;
 
-@EnableAutoConfiguration
 @Configuration
 public class SchedulerConfig {
 
     private final ScriptJobFactory scriptJobFactory;
-    private ApplicationContext applicationContext;
+    private String jdbcUrl;
+    private String jdbcUser;
+    private String jdbcPassword;
 
-    public SchedulerConfig(ScriptJobFactory scriptJobFactory, ApplicationContext applicationContext) {
+    public SchedulerConfig(ScriptJobFactory scriptJobFactory, 
+                            @Value("${spring.datasource.url}") String jdbcUrl,
+                            @Value("${spring.datasource.username") String jdbcUser,
+                            @Value("${spring.datasource.password}") String jdbcPassword) {
         this.scriptJobFactory = scriptJobFactory;
-        this.applicationContext = applicationContext;
+        this.jdbcUrl = jdbcUrl;
+        this.jdbcUser = jdbcUser;
+        this.jdbcPassword = jdbcPassword;
     }
 
     @Bean
     SchedulerFactoryBean schedulerFactoryBean() {
         SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
-        schedulerFactoryBean.setConfigLocation(new ClassPathResource("quartz.properties"));
+        schedulerFactoryBean.setConfigLocation(new ClassPathResource("application.yml"));
         schedulerFactoryBean.setJobFactory(scriptJobFactory);
         return schedulerFactoryBean;
     }
@@ -46,34 +45,14 @@ public class SchedulerConfig {
     @QuartzDataSource
     DataSource quartzDataSource() {
         DataSource dataSource = DataSourceBuilder.create()
-                .driverClassName("org.postgresql.Driver")
-                .url("jdbc:postgresql://192.168.86.42:5432/webexecuter")
-                .username("postgres")
-                .password("postgres")
+                .url(jdbcUrl)
+                .username(jdbcUser)
+                .password(jdbcPassword)
                 .build();
         // schema init
         Resource initSchema = new ClassPathResource("quartz_tables_postgres.sql");
         DatabasePopulator databasePopulator = new ResourceDatabasePopulator(initSchema);
         DatabasePopulatorUtils.execute(databasePopulator, dataSource);
         return dataSource;
-    }
-
-    @Bean
-    SpringBeanJobFactory springBeanJobFactory() {
-        AutoWiringSpringBeanJobFactory jobFactory = new AutoWiringSpringBeanJobFactory();
-        jobFactory.setApplicationContext(applicationContext);
-        return jobFactory;
-    }
-
-    @Bean
-    Properties quartzProperties() throws IOException {
-        PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
-        propertiesFactoryBean.setLocation(new ClassPathResource("application.properties"));
-
-        Properties props = new Properties();
-        props.put("org.quartz.jobStore.driverDelegateClass", "org.quartz.impl.jdbcjobstore.PostgreSQLDelegate");
-        propertiesFactoryBean.setProperties(props);
-        propertiesFactoryBean.afterPropertiesSet();
-        return propertiesFactoryBean.getObject();
     }
 }
